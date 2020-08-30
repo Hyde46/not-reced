42f1616ba1cf19577b20bd20c358b53e2c2af9b1
Hyde46 <denis.heid@protonmail.com>
2020-08-30 18:48:44 +0200

    Add bot navigation

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Typography from '@material-ui/core/Typography';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
-
 import EdhRecSearch from './EdhRecSearch.js';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import GitHubIcon from '@material-ui/icons/GitHub';
 
 const theme = createMuiTheme({
   typography: {
@@ -43,26 +45,29 @@ function App() {
   }
 
   return (
-    
+
     <ThemeProvider theme={theme}>
-      <div className="App" style={{margin: "125px"}}>
+      <div className="App" style={{ margin: "125px" }}>
         <center>
 
           <Typography variant="h5" component="h5">
-            Hipster EDH Deck helper
+            Underdog EDH Deck helper
           </Typography>
 
-          <div style={{marginTop: "55px", marginBottom: "55px"}}>
+          <div style={{ marginTop: "55px", marginBottom: "55px" }}>
             <form noValidate autoComplete="off">
-              <TextField id="outlined-basic2" label="Commander" variant="outlined" style={{marginBottom: "15px"}} onChange={handleCommanderChange}/>
+              <TextField id="outlined-basic2" label="Commander (Optional)" variant="outlined" style={{ marginBottom: "15px" }} onChange={handleCommanderChange} />
             </form>
             <form noValidate autoComplete="off">
               <TextField value={query} id="outlined-basic" label="Cardname" variant="outlined" onChange={handleChange} />
             </form>
           </div>
-          {query && <EdhRecSearch query={query} commanderName={commanderName}/>}
+          {query && <EdhRecSearch query={query} commanderName={commanderName} />}
         </center>
       </div>
+      <BottomNavigation value={""} >
+        <BottomNavigationAction label="Recents" value="recents" icon={<GitHubIcon />} href="https://github.com/Hyde46/not-reced" />
+      </BottomNavigation>
     </ThemeProvider>
   );
 }
--- src/EdhRecSearch.js
+++ src/EdhRecSearch.js
@@ -8,27 +8,31 @@ import {
     useAsyncTaskDelay,
     useAsyncTaskFetch,
     useAsyncTask
-  } from 'react-hooks-async';
+} from 'react-hooks-async';
 
 let COMMANDER = "";
-const fetchEdhRecTask = async ({signal}, name) => {
-    if (COMMANDER === "" || COMMANDER === undefined || COMMANDER === null){
+const fetchEdhRecTask = async ({ signal }, name) => {
+    if (COMMANDER === "" || COMMANDER === undefined || COMMANDER === null) {
         return "";
-    }    
+    }
+    console.log("Looking  commander specific");
     const url = `https://edhrec-json.s3.amazonaws.com/en/commanders/${COMMANDER}.json`;
     const response = await fetch(url, { signal });
     const data = await response.json();
+    console.log("Found specific");
     return data;
-  }
+}
 
-  const fetchRecomendations = async ({signal}, id) => {
-      const url = 'https://raw.githubusercontent.com/Hyde46/not-reced/master/recomendations.txt';
-      const response = await fetch(url, { signal});
-      const data = await response.text();
-      return data;
-  }
+const fetchRecomendations = async ({ signal }, id) => {
+    console.log("Looking for general")
+    const url = 'https://raw.githubusercontent.com/Hyde46/not-reced/master/recomendations.txt';
+    const response = await fetch(url, { signal });
+    const data = await response.text();
+    console.log("Found general");
+    return data;
+}
 
-const sanitizeCardName = function(cardName) {
+const sanitizeCardName = function (cardName) {
     cardName = cardName.toLowerCase();
     // Remove special characters
     cardName = cardName.replace(/[&\/#,+()$~%.'`":*?<>{}]/g, '');
@@ -36,14 +40,14 @@ const sanitizeCardName = function(cardName) {
     cardName = cardName.replace(/[\s]/g, '-');
     return cardName;
 }
-const isCardRecomended = function(body, card) {
+const isCardRecomended = function (body, card) {
     let found_card = false;
     let card_name = ""
     let sanitzed_card = sanitizeCardName(card);
     let cards = body.split('\n');
-    cards.forEach( c => {
+    cards.forEach(c => {
 
-        if (c === sanitzed_card){
+        if (c === sanitzed_card) {
             found_card = true;
             card_name = card;
             return [found_card, card_name, sanitzed_card];
@@ -51,18 +55,19 @@ const isCardRecomended = function(body, card) {
     })
     return [found_card, card_name, sanitzed_card];
 }
-const isCommanderCardRecomended = function(body, card) {
-    console.log("finding commander cards");
-    if ( card === undefined || card === null || card === ""){
+const isCommanderCardRecomended = function (body, card) {
+    if (body.container === undefined || body.container === null) {
+        return [false, "", ""];
+    }
+    if (card === undefined || card === null || card === "") {
         return [false, "", ""];
     }
     let found_card = false;
     let card_name = ""
     let sanitzed_card = sanitizeCardName(card);
-    console.log(body);
     body.container.json_dict.cardlists.forEach(json_dict => {
         json_dict.cardviews.forEach((e) => {
-            if(e.sanitized === sanitzed_card){
+            if (e.sanitized_wo === sanitzed_card) {
                 card_name = e.name;
                 found_card = true;
             }
@@ -71,58 +76,59 @@ const isCommanderCardRecomended = function(body, card) {
     return [found_card, card_name, sanitzed_card];
 }
 
-const prettyResponse = function(cardFound) {
-    let base_url = "https://edhrec.com/cards/"
-    let card_link = base_url + cardFound[2];
-    if (cardFound[0]) {
+const prettyResponse = function (cardFound, cardData, commanderCardData) {
+    let data = commanderCardData[1] !== "" ? commanderCardData : cardData;
+    let base_url = "https://edhrec.com/cards/";
+    let card_link = base_url + data[2];
+    if (cardFound) {
         return (
             <div>
-                <CloseIcon style={{ color:"#ED2939" , fontSize: 40}} />
-                <p> 
-                    <a href={card_link} target="_blank">{cardFound[1]} </a>
+                <CloseIcon style={{ color: "#ED2939", fontSize: 40 }} />
+                <p>
+                    <a href={card_link} target="_blank">{data[1]} </a>
                     recomended by EDHRec!
                 </p>
             </div>
-            );
+        );
     } else {
         return (
             <div>
-                <DoneAllIcon color="primary" style={{ fontSize: 40}}/>
+                <DoneAllIcon color="primary" style={{ fontSize: 40 }} />
                 <p> Card not recomended by EDHRec!</p>
             </div>
-            );
+        );
     }
 }
-  
+
 
 const EdhRecSearch = ({ query, commanderName }) => {
 
-    commanderName = commanderName === undefined || commanderName === null ?  "" : commanderName;
+    commanderName = commanderName === undefined || commanderName === null ? "" : commanderName;
     COMMANDER = sanitizeCardName(commanderName);
     const delayCommanderTask = useAsyncTaskDelay(500);
     const fetchCommanderRecs = useAsyncTask(fetchEdhRecTask);
     const combinedCommanderTask = useAsyncCombineSeq(delayCommanderTask, fetchCommanderRecs);
     useAsyncRun(combinedCommanderTask);
-   
+
 
     const delayTask = useAsyncTaskDelay(500);
     const fetchTop = useAsyncTask(fetchRecomendations);
     const combinedTask = useAsyncCombineSeq(delayTask, fetchTop);
     useAsyncRun(combinedTask);
 
-    if (commanderName !== ""){
-        if (delayCommanderTask.pending) return <CircularProgress/>;
+    if (commanderName !== "") {
+        if (delayCommanderTask.pending) return <CircularProgress />;
         if (fetchCommanderRecs.aborted) return <div>...</div>;
         if (fetchCommanderRecs.error) return <div>Error while fetching data...</div>;
         if (fetchCommanderRecs.pending) return <div>Aborted</div>;
     }
-    if (delayTask.pending) return <CircularProgress/>;
+    if (delayTask.pending) return <CircularProgress />;
     if (fetchTop.aborted) return <div>...</div>;
     if (fetchTop.error) return <div>Error while fetching data...</div>;
     if (fetchTop.pending) return <div>Aborted</div>;
     const cardFound = isCardRecomended(fetchTop.result, query);
-    const commanderCardFound = isCommanderCardRecomended(fetchCommanderRecs.result, COMMANDER);
-    return(prettyResponse(cardFound || commanderCardFound));
+    const commanderCardFound = isCommanderCardRecomended(fetchCommanderRecs.result, query);
+    return (prettyResponse(cardFound[0] || commanderCardFound[0], cardFound, commanderCardFound));
 }
 
 export default EdhRecSearch;
